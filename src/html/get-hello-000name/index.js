2ae65c7aef0b8b8b209a9047029a21c0dda4379c
Drew Robinson <drew.robinson@gmail.com>
2018-08-17 14:06:40 +0800

    Corrected hello function, added some more package.json info

--- src/html/get-hello-000name/index.js
+++ src/html/get-hello-000name/index.js
@@ -1,21 +1,21 @@
 let begin = require('@architect/functions')
 
-let defaultHTML = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-  </head>
-  <body style="font-family: sans-serif;">
-    <h1>Hello there ${name} !</h1>
-  </body>
-</html>
-`
-
 function route(req, res) {
   console.log(JSON.stringify(req, null, 2))
   let name = req.params.name ? req.params.name : "brave world";
+  let defaultHTML = `
+    <!doctype html>
+    <html lang=en>
+      <head>
+        <meta charset=utf-8>
+        <title>Hi!</title>
+      </head>
+      <body style="font-family: sans-serif;">
+        <h1>Hello there ${name} !</h1>
+      </body>
+    </html>
+  `
+
   res({
     html: defaultHTML
   })
