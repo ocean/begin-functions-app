ca26ecb1399b65030d492a12582b214b581812dc
Drew Robinson <drew.robinson@gmail.com>
2018-08-21 09:45:26 +0800

    Added request parameter

--- src/html/get-hello-000name/index.js
+++ src/html/get-hello-000name/index.js
@@ -1,23 +1,22 @@
 let begin = require('@architect/functions')
 
-// TODO change defaultHTML
-let defaultHTML = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-  </head>
-  <body style="font-family: sans-serif;">
-    <h1>Hello world!</h1>
-  </body>
-</html>
-`
-
 function route(req, res) {
   console.log(JSON.stringify(req, null, 2))
+  let name = (req.params.name.length > 0) ? req.params.name : "you there";
+  let responseHTML = `
+    <!doctype html>
+    <html lang=en>
+      <head>
+        <meta charset=utf-8>
+        <title>Hi!</title>
+      </head>
+      <body style="font-family: sans-serif;">
+        <h1>Hello {{ name }}</h1>
+      </body>
+    </html>
+  `
   res({
-    html: defaultHTML
+    html: responseHTML
   })
 }
 
